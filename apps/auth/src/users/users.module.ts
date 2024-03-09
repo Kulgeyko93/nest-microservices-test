a7902b51ac14255b96a2b52345e923b7954e2690
kulgeyko93 <Kulgeyko93@mail.ru>
2024-03-09 12:18:37 +0300

    feat: add jwt

--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -14,6 +14,7 @@ import { UsersRepository } from './users.repository';
         schema: UserSchema,
       },
     ]),
+
     LoggerModule,
   ],
   controllers: [UsersController],
---
+++ libs/common/src/app-configs/index.ts
@@ -0,0 +1,4 @@
+export * from './env.config';
+export * from './mongodb.config';
+export * from './pino-logger.config';
+export * from './jwt-auth.config';
