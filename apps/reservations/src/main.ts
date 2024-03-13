f90e09234c5e70fb6ac49c24cd6bf4ebbb6e311e
kulgeyko93 <Kulgeyko93@mail.ru>
2024-03-13 09:38:05 +0300

    feat: init notification service

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -5,15 +5,11 @@ import { ZodValidationPipe } from 'nestjs-zod';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
 
-const configService = new ConfigService();
-
 async function bootstrap() {
-  const port = configService.get('PORT') || 7080;
   const app = await NestFactory.create(ReservationsModule);
-  configService.get('PAYMENTS_HOST');
-  configService.get('PAYMENTS_PORT');
-  configService.get('PAYMENTS_HOST');
-  configService.get('PAYMENTS_PORT');
+  const configService = app.get(ConfigService);
+
+  const port = configService.get('PORT') || 7080;
 
   app.useGlobalPipes(new ZodValidationPipe());
   app.useLogger(app.get(Logger));
