9500eafd15ab8b11bb32bb37a2b91e0ebf848ca6
kulgeyko93 <Kulgeyko93@mail.ru>
2024-03-14 10:42:51 +0300

    feat: auth docker file

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -2,7 +2,7 @@ import { ConfigService } from '@nestjs/config';
 import { CreateUserDto } from './dto/create-user.dto';
 import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
-import * as bcrypt from 'bcrypt';
+import * as bcryptjs from 'bcryptjs';
 import { GetUserDto } from './dto/get-user.dto';
 
 @Injectable()
@@ -16,7 +16,7 @@ export class UsersService {
   }
 
   async create(createUserDto: CreateUserDto) {
-    const hashedPassword = await bcrypt.hash(createUserDto.password, +this.SALT);
+    const hashedPassword = await bcryptjs.hash(createUserDto.password, +this.SALT);
     return this.usersRepository.create({
       ...createUserDto,
       password: hashedPassword,
@@ -34,7 +34,7 @@ export class UsersService {
       throw new NotFoundException("User doest't exist");
     }
 
-    const passwordIsValid = await bcrypt.compare(password, user.password);
+    const passwordIsValid = await bcryptjs.compare(password, user.password);
 
     if (!passwordIsValid) {
       throw new UnauthorizedException('Credentials are not valid.');
