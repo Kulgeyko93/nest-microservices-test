a343d84047889fc1531b50694c47b6ae5439e6d1
kulgeyko93 <Kulgeyko93@mail.ru>
2024-03-28 11:30:06 +0300

    feat: init app

--- libs/common/src/app-configs/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './env.config';
-export * from './mongodb.config';
-export * from './pino-logger.config';
-export * from './jwt-auth.config';
--- libs/common/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './database';
-export * from './logger';
-export * from './app-configs';
-export * from './auth';
-export * from './constants';
-export * from './dto';
