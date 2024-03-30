eae70dcf018ba211c022213e63e8739e75c7701d
kulgeyko93 <Kulgeyko93@mail.ru>
2024-03-30 14:54:09 +0300

    feat: auth jwt logic with refresh tocken

--- apps/account/core/configs/env.config.ts
+++ apps/account/core/configs/env.config.ts
@@ -5,6 +5,10 @@ import * as path from 'node:path';
 export const envSchema = z.object({
   PORT: z.string(),
   JWT_SECRET: z.string(),
+  JWT_ACCESS_SECRET: z.string(),
+  JWT_ACCESS_EXPIRES: z.string(),
+  JWT_REFRESH_SECRET: z.string(),
+  JWT_REFRESH_EXPIRES: z.string(),
 });
 
 export const accountEnvConfig = (): ConfigModuleOptions => ({
