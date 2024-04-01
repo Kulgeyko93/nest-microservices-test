c5c1d0cc7b2f5a00f159899cb817e981cf33a577
kulgeyko93 <Kulgeyko93@mail.ru>
2024-04-01 12:23:36 +0300

    feat: add auth context

--- apps/account/src/user/user.module.ts
+++ apps/account/src/user/user.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { UserRepository } from './repositories/user.repository';
-import { UserService } from './user.service';
 import { DatabaseModule } from '@lib/common';
+import { Module } from '@nestjs/common';
 import { UserModel } from './models/user.model';
+import { UserRepository } from './repositories/user.repository';
 import { UserResolver } from './user.resolver';
+import { UserService } from './user.service';
 
 @Module({
   imports: [DatabaseModule, DatabaseModule.forFeature([UserModel])],
