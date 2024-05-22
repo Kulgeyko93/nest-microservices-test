3b668885aaf5229b8d08cc73b172e1c70580d243
kulgeyko93 <Kulgeyko93@mail.ru>
2024-05-22 15:45:52 +0300

    fix: auth

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -3,8 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { gatewayEnvConfig } from './core/configs/env.config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloGatewayDriverConfig } from '@nestjs/apollo';
-import { ClientsModule } from '@nestjs/microservices';
-import { clientModuleConfig } from './core/configs/client-module.config';
 import { apolloGatewayDriverConfig } from './core/configs/apollo-gateway-driver.config';
 
 @Module({
@@ -13,7 +11,6 @@ import { apolloGatewayDriverConfig } from './core/configs/apollo-gateway-driver.
     GraphQLModule.forRoot<ApolloGatewayDriverConfig>(
       apolloGatewayDriverConfig(),
     ),
-    ClientsModule.registerAsync(clientModuleConfig()),
   ],
   controllers: [],
   providers: [],
