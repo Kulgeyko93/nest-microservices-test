7823a506ec17e9156adabd9d5db7196c36fe534e
kulgeyko93 <Kulgeyko93@mail.ru>
2024-05-26 12:49:50 +0300

    feat: upload files to minio

--- apps/feed/src/core/configs/minio.config.ts
+++ apps/feed/src/core/configs/minio.config.ts
@@ -5,7 +5,7 @@ export const getMinioConfig = () => ({
   inject: [ConfigService],
   useFactory: (configService: ConfigService) => ({
     endPoint: configService.get('MINIO_ENDPOINT'),
-    port: configService.get('MINIO_PORT'),
+    port: +configService.get('MINIO_PORT'),
     useSSL: false,
     accessKey: configService.get('MINIO_ACCESS_KEY'),
     secretKey: configService.get('MINIO_ACCESS_SECRET'),
--- apps/feed/src/modules/minio/minio.module.ts
+++ apps/feed/src/modules/minio/minio.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MinioModule } from 'nestjs-minio-client';
 import { getMinioConfig } from '../../core/configs/minio.config';
+import { MinioClientService } from './minio-client.service';
 
 @Module({
   imports: [MinioModule.registerAsync(getMinioConfig())],
+  providers: [MinioClientService],
+  exports: [MinioClientService],
 })
 export class MinioStorageModule {}
