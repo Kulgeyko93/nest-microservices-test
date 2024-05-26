6da543aff218950888c02f3154fa157dbd92619b
kulgeyko93 <Kulgeyko93@mail.ru>
2024-05-26 11:47:34 +0300

    feat: add secrets

--- apps/feed/src/core/configs/env.config.ts
+++ apps/feed/src/core/configs/env.config.ts
@@ -6,6 +6,11 @@ export const envSchema = z.object({
   HTTP_PORT: z.string(),
   ACCOUNT_HOST: z.string(),
   ACCOUNT_PORT: z.string(),
+
+  MINIO_ROOT_USER: z.string(),
+  MINIO_ROOT_PASSWORD: z.string(),
+  MINIO_ACCESS_KEY: z.string(),
+  MINIO_ACCESS_SECRET: z.string(),
 });
 
 export const feedEnvConfig = (): ConfigModuleOptions => ({
