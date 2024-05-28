2826605a2ceb5aecf4fe24cfc564aa7a616acc8b
kulgeyko93 <Kulgeyko93@mail.ru>
2024-05-28 15:19:50 +0300

    feat: abstract repository

--- apps/account/src/account.module.ts
+++ apps/account/src/account.module.ts
@@ -1,18 +1,23 @@
 import { ApolloFederationDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+
 import { AuthModule } from './auth/auth.module';
-import { accountEnvConfig } from './auth/core/configs/env.config';
+import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
+
+import { accountEnvConfig } from './auth/core/configs/env.config';
 import { accountGraphqlConfig } from './auth/core/configs/graphql-account.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot(accountEnvConfig()),
     GraphQLModule.forRoot<ApolloFederationDriverConfig>(accountGraphqlConfig()),
-    UserModule,
     AuthModule,
+    PostModule,
+    UserModule,
   ],
 })
 export class AccountModule {}
--- apps/account/src/post/post.resolver.ts
+++ apps/account/src/post/post.resolver.ts
@@ -1,23 +1,25 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { PostRepository } from './repositories/post.repository';
-import { PostEntity } from '@lib/common';
+import { CurrentGqlUser, IUserEntityContract, PostEntity } from '@lib/common';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthAccessTokenGuard } from '../auth/guards/gql-auth-access-token.guard';
 
 @Resolver(() => PostEntity)
 export class PostResolver {
   constructor(private readonly postRepository: PostRepository) {}
 
-  // @Query(() => [PostEntity], { name: 'posts' })
-  // findAll() {
-  //   return this.postRepository.find();
-  // }
+  @Query(() => [PostEntity], { name: 'posts' })
+  @UseGuards(GqlAuthAccessTokenGuard)
+  findAll(@CurrentGqlUser() user: IUserEntityContract) {
+    return this.postRepository.find({ userId: user.id });
+  }
 
-  // @Query(() => PostEntity, { name: 'posts' })
-  // findOne(@Args('id', { type: () => String }) id: string) {
-  //   return this.postRepository.findOne({ id });
-  // }
-
-  // @Mutation(() => PostEntity)
-  // removeUser(@Args('id', { type: () => String }) id: string) {
-  //   return this.postRepository.remove({ id });
-  // }
+  @Query(() => PostEntity, { name: 'post' })
+  @UseGuards(GqlAuthAccessTokenGuard)
+  findOne(
+    @Args('id', { type: () => String }) id: string,
+    @CurrentGqlUser() user: IUserEntityContract,
+  ) {
+    return this.postRepository.findOne({ id, userId: user.id });
+  }
 }
