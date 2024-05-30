25a9e5d291e98ee2e7342e068b13fce81e6e290a
kulgeyko93 <Kulgeyko93@mail.ru>
2024-05-30 10:09:09 +0300

    feat: change logic of headers

--- apps/account/src/auth/guards/gql-auth-access-token.guard.ts
+++ apps/account/src/auth/guards/gql-auth-access-token.guard.ts
@@ -2,7 +2,7 @@ import { ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
-import { Observable } from 'rxjs';
+
 import { JwtStrategyName } from '../core/constants';
 
 @Injectable()
@@ -13,20 +13,21 @@ export class GqlAuthAccessTokenGuard extends AuthGuard(JwtStrategyName.JWT) {
 
   public getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
-    return ctx.getContext().req;
+    const req = ctx.getContext().req;
+    return req;
   }
 
-  public canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    const isPublic = this.reflector.getAllAndOverride('isPublic', [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-    if (isPublic) {
-      return true;
-    }
+  // public canActivate(
+  //   context: ExecutionContext,
+  // ): boolean | Promise<boolean> | Observable<boolean> {
+  //   const isPublic = this.reflector.getAllAndOverride('isPublic', [
+  //     context.getHandler(),
+  //     context.getClass(),
+  //   ]);
+  //   if (isPublic) {
+  //     return true;
+  //   }
 
-    return super.canActivate(context);
-  }
+  //   return super.canActivate(context);
+  // }
 }
