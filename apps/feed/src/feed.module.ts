dc0cb83a6b6ff6049288b0703c2bfe6a31e3ed3e
kulgeyko93 <Kulgeyko93@mail.ru>
2024-06-20 16:25:05 +0300

    fix: remove  unusable code

--- apps/feed/src/feed.module.ts
+++ apps/feed/src/feed.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ClientsModule } from '@nestjs/microservices';
-import { clientModuleConfig } from './core/configs/client-module.config';
 import { feedEnvConfig } from './core/configs/env.config';
 import { MinioStorageModule } from './modules/minio/minio.module';
 import { UploadModule } from './modules/upload/upload.module';
@@ -9,7 +7,6 @@ import { UploadModule } from './modules/upload/upload.module';
 @Module({
   imports: [
     ConfigModule.forRoot(feedEnvConfig()),
-    ClientsModule.registerAsync(clientModuleConfig()),
     MinioStorageModule,
     UploadModule,
   ],
