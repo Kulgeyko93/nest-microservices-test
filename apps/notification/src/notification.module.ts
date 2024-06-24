024603ca50b22227a75a1dd922f9900c3101c4e3
kulgeyko93 <Kulgeyko93@mail.ru>
2024-06-24 16:25:21 +0300

    feat: init sockets

--- apps/notification/src/notification.module.ts
+++ apps/notification/src/notification.module.ts
@@ -3,9 +3,10 @@ import { NotificationController } from './notification.controller';
 import { NotificationService } from './notification.service';
 import { ConfigModule } from '@nestjs/config';
 import { NotificationEnvConfig } from './core/configs/env.config';
+import { GatewayModule } from './gateway/gateway.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(NotificationEnvConfig())],
+  imports: [ConfigModule.forRoot(NotificationEnvConfig()), GatewayModule],
   controllers: [NotificationController],
   providers: [NotificationService],
 })
