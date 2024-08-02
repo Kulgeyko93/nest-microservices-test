5a03f1564fffbb559ed6724abec2ecd7829f6840
kulgeyko93 <kulgeyko93@mail.ru>
2024-08-02 09:18:32 +0300

    fix: env path

--- apps/posts-service/src/core/configs/env.config.ts
+++ apps/posts-service/src/core/configs/env.config.ts
@@ -9,7 +9,7 @@ export const envSchema = z.object({
 export const postEnvConfig = (): ConfigModuleOptions => ({
   isGlobal: true,
   validate: (env) => envSchema.parse(env),
-  envFilePath: path.join(process.cwd(), 'envs', '.account.env'),
+  envFilePath: path.join(process.cwd(), 'envs', '.post.env'),
 });
 
 export type PostEnv = z.infer<typeof envSchema>;
